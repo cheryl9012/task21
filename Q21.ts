f7b3c0e14f96f283f1d2525e9d4ed6f01e2fb9ce
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-23 21:30:34 +0500

    Add files via upload

---
+++ Q21.ts
@@ -0,0 +1,6 @@
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+
+let bioData = { Name : " Sheryl", Father_name : "Peter" , Age : 30};
+
+console.log(bioData);
